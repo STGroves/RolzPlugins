76306c28e011fc412ac5603d34b55acb97ff0d4e
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-23 17:13:37 +0000

    Added missing chat initiator

--- OldChat.js
+++ OldChat.js
@@ -3,6 +3,8 @@ document.getElementById("table-dock-resizer").remove();
 document.getElementById("table-dock-container").remove();
 
 if (!DM.userdata.hasOwnProperty("p_chat"))
-  TableUI.pane.open({id:"chat", x:0, y:window.innerHeight - 300, width:750, height:300, title: "Chat", noclose: true, content: $('#t-chat-view').html(), style: "overflow:hidden;"});
+  TableUI.pane.open({id:"chat", x: 0, y: window.innerHeight - 300, w: 750, h: 300, title: "Chat", noclose: true, content: $('#t-chat-view').html(), style: "overflow:hidden;"});
 else
   TableUI.pane.open(DM.userdata.p_chat);
+
+Chat.init();
