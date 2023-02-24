a9e3f7ad2ebd6df035dce37fd53ab42e266311a4
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-24 23:58:34 +0000

    Fixed assignment

--- RoomLogo.js
+++ RoomLogo.js
@@ -1,6 +1,6 @@
 try {
 let image = document.createElement("img");
-let foundImage = custom_token_list.find(x => x.name = "RoomLogo.png");
+let foundImage = custom_token_list.find(x => x.name === "RoomLogo.png");
 
 if(!foundImage)
   throw "Cannot find Room logo!";
