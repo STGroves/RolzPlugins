6173c408d2674f1b8c14b7a1a19655a4937d5af9
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-24 02:53:14 +0000

    Created OldLedger.js

--- OldLedger.js
+++ OldLedger.js
@@ -1,12 +1,18 @@
-if (!DM.userdata.hasOwnProperty("p_tbl_ledger"))
-  TableUI.pane.open({
-    id : 'tbl_ledger',
-    title : 'Ini. Ledger',
-    x : 50,
-    y : 75,
-    w : 50,
-    h : 25,
-    load : '/table/ledger?room_id='+encodeURIComponent(WSConnection.options.room_id),
-  });
-else
-  TableUI.pane.open(DM.userdata.p_tbl_ledger);
+import ToolbarUtilities from "./PluginUtilities/ToolbarUtilities";
+
+function DisplayLedger() {
+  if (!DM.userdata.hasOwnProperty("p_tbl_ledger"))
+    TableUI.pane.open({
+      id : 'tbl_ledger',
+      title : 'Ini. Ledger',
+      x : 50,
+      y : 75,
+      w : 50,
+      h : 25,
+      load : '/table/ledger?room_id='+encodeURIComponent(WSConnection.options.room_id),
+    });
+  else
+    TableUI.pane.open(DM.userdata.p_tbl_ledger);
+}
+
+ToolbarUtilities.createButton("table-toolbar", "ledger", "Ledger", "list-ul", DisplayLedger);
