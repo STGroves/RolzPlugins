ca0665a7479699d7ff4d497447eb6b3748b5c289
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-24 23:34:38 +0000

    Created RoomLogo

---
+++ RoomLogo.js
@@ -0,0 +1,14 @@
+try {
+let image = document.createElement("img");
+let foundImage = custom_token_list.find(x => x.name = "RoomLogo.png");
+
+if(!!foundImage)
+  throw "Cannot find Room logo!";
+
+image.src = foundImage.url;
+image.style.width = "100%";
+
+document.getElementById("room_info").replaceChildren([image]);
+} catch(e) {
+  console.error(e);
+}
