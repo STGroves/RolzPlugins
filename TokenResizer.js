8065f99e724d5d10db772b2a3d06600d10c9997e
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-28 17:45:35 +0000

    Created TokenResizer.js

---
+++ TokenResizer.js
@@ -0,0 +1,17 @@
+setInterval(() => {loadCSS();}, 500);
+
+const sizes = ["1", "1_5", "2", "3", "4"];
+let style = document.getElementById("map-dynamic-style");
+
+function loadCSS() {
+  for (const [_, value] of Object.entries(DM.data.tokens)) {
+    if (!sizes.includes(value.sz) && style.innerHTML.search(".token-" + token.sz + "x") === -1)
+      addClass(value.sz);
+  }
+}
+
+function addClass(size) {
+  let sizeNum = Number(string.replace("_", "."));
+  style.innerHTML += `.token-${size}x { width: ${sizeNum * DM.tile_size }px; height: ${sizeNum * DM.tile_size}px; }
+  .token-${size}x .glyph { font-size: ${sizeNum * DM.tile_size / 1.3}px; }`;
+}
