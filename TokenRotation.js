48cd7874bcc734aef567766ae76dd8a67549d9e9
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-28 01:43:12 +0000

    Updated Promise

--- TokenRotation.js
+++ TokenRotation.js
@@ -1,16 +1,18 @@
 import HTMLUtilities from "./PluginUtilities/HTMLUtilities.js"
 
 DM.tools.tokens.edit_properties = function(id) {
-  new Promise((resolve) => {DM.selected_id = id;
-  resolve(TableUI.pane.open({
-    id : 'tbl_prop_edit',
-    title : 'Token Properties',
-    x : 75,
-    y : 10,
-    w : 25,
-    h : 60,
-    load : '/table/token-edit?room_id='+WSConnection.options.room_id+'&map_id=current&token_id='+encodeURIComponent(id),
-  }));
+  new Promise((resolve) => {
+    DM.selected_id = id;
+    TableUI.pane.open({
+      id : 'tbl_prop_edit',
+      title : 'Token Properties',
+      x : 75,
+      y : 10,
+      w : 25,
+      h : 60,
+      load : '/table/token-edit?room_id='+WSConnection.options.room_id+'&map_id=current&token_id='+encodeURIComponent(id),
+    })
+    resolve();
   }).then(x => 
     document.getElementById("view-tbl_prop_edit-content").querySelector("rot-buttons").replaceChildren(HTMLUtilities.createSlider(
     {
