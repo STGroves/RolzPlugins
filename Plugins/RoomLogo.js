a7653f081dabec2cce7ab52946f6d7d7441e9517
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-28 20:08:48 +0000

    Edited RoomLogo to override room info rendering

--- Plugins/RoomLogo.js
+++ Plugins/RoomLogo.js
@@ -1,14 +1,16 @@
-try {
-let image = document.createElement("img");
-let foundImage = custom_token_list.find(x => x.name === "RoomLogo.png");
-
-if(!foundImage)
-  throw "Cannot find Room logo!";
-
-image.src = foundImage.url;
-image.style.width = "100%";
-
-document.getElementById("room_info").replaceChildren(image);
-} catch(e) {
-  console.error(e);
+PartyList.render_room_info = function() {
+  try {
+    let image = document.createElement("img");
+    let foundImage = custom_token_list.find(x => x.name === "RoomLogo.png");
+    
+    if(!foundImage)
+      throw "Cannot find Room logo!";
+    
+    image.src = foundImage.url;
+    image.style.width = "100%";
+    
+    document.getElementById("room_info").replaceChildren(image);
+  } catch(e) {
+    console.error(e);
+  }
 }
