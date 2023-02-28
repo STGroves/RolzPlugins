f6b0dda1ea7936d09aaa867f1d6efa84e13b8eec
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-28 16:42:50 +0000

    Added conversion from HTMLCollection to array

--- DockMinimiser.js
+++ DockMinimiser.js
@@ -6,7 +6,7 @@ TableUI.dock.toggle = function(id) {
     $('#tgb-'+id).addClass('inactive');
 
   const list = document.getElementById("table-dock-tabs").children;
-  const allEqual = list.every((value) => value.classList.contains("inactive") === true);
+  const allEqual = Array.from(list).every((value) => value.classList.contains("inactive") === true);
 
   if (allEqual) {
     document.getElementById("table-dock-tabs").style.bottom = 0;
