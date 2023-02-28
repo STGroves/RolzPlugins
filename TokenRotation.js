38a531e499c7a07f6805810c18099688648786a2
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-02-28 01:40:00 +0000

    Added checks to ensure correct element selection

--- TokenRotation.js
+++ TokenRotation.js
@@ -12,7 +12,7 @@ DM.tools.tokens.edit_properties = function(id) {
     load : '/table/token-edit?room_id='+WSConnection.options.room_id+'&map_id=current&token_id='+encodeURIComponent(id),
   }));
   }).then(x => 
-  document.getElementsByClassName("rot-buttons")[0].replaceChildren(HTMLUtilities.createSlider(
+    document.getElementById("view-tbl_prop_edit-content").querySelector("rot-buttons").replaceChildren(HTMLUtilities.createSlider(
     {
       id: "rotation",
       min: 0,
