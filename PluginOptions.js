6ce049009dc2a67340f8e56eaab9dc1619c01a11
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-03-01 01:23:16 +0000

    Added missing default

--- PluginOptions.js
+++ PluginOptions.js
@@ -1,3 +1,4 @@
+const elem = document.getElementsByTagName("body")[0];
 let scriptElem = document.createElement("script");
 scriptElem.type = "module";
 scriptElem.src = `https://stgroves.github.io/RolzPlugins/PluginLoader.js`;
