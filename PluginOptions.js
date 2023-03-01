23241181e46b2ea6f2b2ceda4ec823eab6e6631a
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-03-01 11:11:41 +0000

    Fixed path

--- PluginOptions.js
+++ PluginOptions.js
@@ -2,5 +2,5 @@ import PluginLoader from "https://stgroves.github.io/RolzPlugins/PluginLoader.js
 
 DM.data.plugins = PluginLoader.init([
   "Plugins/DockMinimiser",
-  "Plugin/OldChat"
+  "Plugins/OldChat"
 ]);
