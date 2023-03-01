736bb0e295f49c89b83cdadfb603ed3f6eb047cd
Sam Groves <18527147+STGroves@users.noreply.github.com>
2023-03-01 01:15:44 +0000

    Test

--- PluginOptions.js
+++ PluginOptions.js
@@ -1,6 +1,10 @@
 import PluginLoader from "https://stgroves.github.io/RolzPlugins/PluginLoader.js";
 
-let Plugins = PluginLoader.init([
-  {type: "Plugins/DockMinimiser", initialFunc: DockMinimiser}
-]);
-console.log("Run!");
+let Plugins;
+
+function loadOptions() {
+  Plugins = PluginLoader.init([
+    {type: "Plugins/DockMinimiser", initialFunc: DockMinimiser}
+  ]);
+  console.log("Run!");
+}
